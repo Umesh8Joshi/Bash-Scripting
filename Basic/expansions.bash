ceee15a6f28bd789db5432e39338f3456125cd69
Umesh Joshi <umesh14el007@gmail.com>
2018-06-02 15:06:35 +0530

    bash basic finished

---
+++ Basic/expansions.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+R=$(( $1 * ($2+$3) ))
+echo project-$R/{src,dest,test}/{index,util}.js
---
+++ Basic/if.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+if [[ $1 -lt 0 || $1 -gt 24 ]]; then
+	echo "Error!"
+elif [[ $1 -lt 12 ]]; then
+		echo "Good Morning!"
+	elif [[ $1 -ge 12 && $1 -lt 18 ]]; then
+			echo "Good afternoon!"
+		elif [[ $1 -gt 18 ]]; then
+				echo "Good evening"
+fi
